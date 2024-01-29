0975792857069b0bac260fec60a275910c886b1e
Sergei <qaserge@gmail.com>
2024-01-29 12:13:45 -0500

    Open-close logic implemented via state

--- src/components/AccordionItem.jsx
+++ src/components/AccordionItem.jsx
@@ -1,10 +1,18 @@
+import { useState } from "react";
+
 export default function AccordionItem({ num, title, text }) {
+  const [open, setOpen] = useState(false);
+  function handleToggle() {
+    setOpen((open) => !open);
+  }
+
   return (
-    <div className="item">
+    <div className={`item ${open ? "open" : ""}`} onClick={handleToggle}>
       <p className="number">{num}</p>
       <p className="text">{title}</p>
-      <p className="icon">-</p>
-      <div className="content-box">{text}</div>
+      <p className="icon">{open ? "-" : "+"}</p>
+
+      {open && <div className="content-box">{text}</div>}
     </div>
   );
 }
